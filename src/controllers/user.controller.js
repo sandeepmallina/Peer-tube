5fc2877153fc7af04a8926be15f0cbc924141503
sandeepmallina <mallinasandeep21@gmail.com>
2024-01-27 18:01:47 +0530

    Add controls to the user control

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,69 @@
+import { User } from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/aysncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // get user details from frontend
+  // validation — not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object — create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { firstName, username, email, password } = req.body;
+
+  //   some takes an array and traverses every field every value and check the given condition if it satisfies it returns
+  //   true .we can use map here but map returns an array
+  if (
+    [firstName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  //Heres the username or email already exists he will throw an error that username or already exists
+  const userExists = await User.findOne({ $or: [{ username }, { email }] });
+  if (userExists) {
+    throw new ApiError(406, "Username or email already exists");
+  }
+
+  //   res.files here is provided by the middleware  multer
+  console.log(res.files);
+  const avatarLocalpath = res.files?.avatar[0]?.path;
+
+  if (!avatarLocalpath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+  //  Here cloudinary will return a success object that contains url
+  const avatar = await uploadOnCloudinary(avatarLocalpath);
+  const coverImageLocalpath = res.files?.coverImage[0]?.path;
+
+  let coverImage;
+  if (coverImageLocalpath) {
+    coverImage = await uploadOnCloudinary(avatarLocalpath);
+  }
+  if (!avatar) {
+    throw new ApiError(402, "check the Avatar file ");
+  }
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage.url || "",
+    username: username.toLowerCase(),
+    email,
+    password,
   });
+
+  const createdUser = await User.findOne(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    new ApiError(500, "Something went wrong while creating user");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(201, createdUser, "user successfully created"));
 });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 // Her register user is
 //  a controller used to define which function should be executed when a particular route is hit
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  // here upload  is a middleware so it so it is exported from the malter so malter is used to upload files
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default router;
