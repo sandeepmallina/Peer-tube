751000663a047bb1e95109fc3ae7c825d7308908
sandeepmallina <mallinasandeep21@gmail.com>
2024-01-29 23:45:16 +0530

    Add subscription model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model.js";
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, //one who is subscribing
+      ref: User,
+    },
+    channel: {
+      type: Schema.Types.ObjectId, //one who is subscriber is subscribing to
+      ref: User,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
